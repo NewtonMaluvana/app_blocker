1124dcbfa1751b7c8300e26c55d598ecd8de1b41
unknown <andzanimaluvana@gmail.com>
2026-04-08 04:06:57 +0200

    adding nav bar

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class color {
   static const Color bgColor = Color.fromARGB(255, 206, 206, 206);
   static const Color bgColor2 = Color.fromARGB(255, 255, 255, 255);
-  static const Color btnColor = Color.fromARGB(255, 120, 32, 187);
+  static const Color btnColor = Color.fromARGB(255, 18, 72, 179);
   static const Color colorText = Color.fromARGB(255, 255, 255, 255);
   static const Color colorText2 = Color.fromARGB(255, 194, 194, 194);
 }
