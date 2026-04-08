b503f40bbab2fb0ae7d37a229862b4a11321b6d5
unknown <andzanimaluvana@gmail.com>
2026-04-08 03:31:55 +0200

    create get started button on the welcome page

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class color {
+  static const Color bgColor = Color.fromARGB(255, 206, 206, 206);
+  static const Color bgColor2 = Color.fromARGB(255, 255, 255, 255);
+  static const Color btnColor = Color.fromARGB(255, 120, 32, 187);
+}
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_blocker/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -12,23 +13,78 @@ class _WelcomePage extends State<WelcomePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Container(decoration: BoxDecoration(color: Colors.blue)),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 131, 79, 20),
+          child: Container(
+            decoration: BoxDecoration(color: color.btnColor),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    decoration: BoxDecoration(color: color.btnColor),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: color.bgColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(45),
+                        bottomLeft: Radius.zero,
+                        bottomRight: Radius.zero,
+                        topRight: Radius.zero,
+                      ),
+                    ),
+                    width: double.infinity,
+
+                    child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Hello There",
+                          style: TextStyle(color: color.btnColor),
+                        ),
+                        Container(
+                          height: 60,
+                          width: 300,
+                          padding: EdgeInsets.only(left: 30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: color.bgColor2,
+                          ),
+                          child: Flex(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            direction: Axis.horizontal,
+                            children: [
+                              Text("Get started"),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: color.btnColor,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                width: 60,
+                                height: 60,
+                                child: Image.asset(
+                                  color: color.bgColor2,
+                                  scale: 0.5,
+                                  width: 10,
+                                  height: 10,
+                                  "images/arrow-right.png",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
