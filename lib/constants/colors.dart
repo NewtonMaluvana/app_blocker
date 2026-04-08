06a8ed4119c52197dfdb5dae2dffc7aae550f642
unknown <andzanimaluvana@gmail.com>
2026-04-08 09:34:01 +0200

    creating premium button

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class color {
-  static const Color bgColor = Color.fromARGB(255, 206, 206, 206);
+  static const Color bgColor = Color.fromARGB(255, 233, 233, 233);
   static const Color bgColor2 = Color.fromARGB(255, 255, 255, 255);
   static const Color btnColor = Color.fromARGB(255, 18, 72, 179);
   static const Color colorText = Color.fromARGB(255, 255, 255, 255);
-  static const Color colorText2 = Color.fromARGB(255, 194, 194, 194);
+  static const Color colorText2 = Color.fromARGB(255, 179, 178, 178);
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_blocker/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -11,28 +12,90 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color.bgColor,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+    return SafeArea(
+      child: Container(
+        color: color.bgColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Flex(
+                      direction: Axis.horizontal,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 200,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Flex(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            direction: Axis.horizontal,
+                            children: [
+                              Icon(Icons.lock, color: Colors.amber),
+                              Text(
+                                "Premium",
+                                style: TextStyle(color: color.colorText),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: color.btnColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
                 ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(color: color.bgColor),
-
-              width: double.infinity,
-            ),
-          ],
+              Container(
+                decoration: BoxDecoration(color: color.bgColor),
+                height: 200,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Flex(
+                        direction: Axis.horizontal,
+                        children: [
+                          Text(
+                            "Features",
+                            style: TextStyle(
+                              color: color.colorText2,
+                              fontSize: 26,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
